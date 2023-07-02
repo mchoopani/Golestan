6d51de62969d77b71caa080244858e2496a8d66a
Mahmood Choopani <mahmood.choopani@divar.ir>
2023-07-02 23:36:20 +0330

    add caching

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const auth = require("./middlewares/auth.js");
 const logger = require("./middlewares/logger.js");
 const cors = require("cors");
+const redis = require("redis");
+
 require("dotenv").config()
 mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST||"127.0.0.1"}:${process.env.DB_PORT||27017}/`)
 .then(()=>{
@@ -13,12 +15,22 @@ mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD
 })
 mongoose.set('debug', true);
 
+let redisClient;
+
+(async () => {
+    redisClient = redis.createClient();
+
+    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+    await redisClient.connect();
+})();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(logger.getLogger());
-app.use(auth.login());
-app.use(auth.authMiddleware);
+app.use(auth.login(redisClient));
+app.use(auth.getAuthMiddleware(redisClient));
 app.use(router);
 
 app.listen(process.env.SERVER_PORT, ()=>{
--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const accessDb = require("../data-access");
 const express = require("express");
 
-const login = () => {
+const login = (redisClient) => {
 
     const router = express.Router();
 
@@ -18,7 +18,13 @@ const login = () => {
                 role: user.__t,
                 college: user.college,
             }, process.env.AUTH_SALT, {expiresIn: `${process.env.AUTH_PASSWORD_EXPIRATION_HOURS || 1}h`});
-            return res.json({token:token, username: user.username, role: user.__t})
+            try {
+                await redisClient.setEx(token, (process.env.AUTH_PASSWORD_EXPIRATION_HOURS || 1) * 3600, "true")
+            } catch (err) {
+                console.log("getting error failed: ", err)
+                return res.status(500).json({message: "internal error happend"})
+            }
+            return res.json({token: token, username: user.username, role: user.__t})
         }
         return res.status(401).json({message: "username or password is wrong."})
     }
@@ -28,36 +34,62 @@ const login = () => {
             const user = jwt.verify(token, process.env.AUTH_SALT);
             return res.status(200).json(user)
         } catch (err) {
-            return res.status(401).json({message:"token is invalid."});
+            return res.status(401).json({message: "token is invalid."});
         }
     }
-    
+
+    const logout = async (req, res) => {
+        const token = req.headers["authorization"]
+        if (!token) {
+            return res.status(200).json({})
+        }
+        try {
+            await redisClient.del(token)
+            return res.status(200).json({})
+        } catch (err) {
+            console.log("getting error failed: ", err)
+            return res.status(500).json({message: "internal error happend"})
+        }
+    }
+
     router.post("/login", login);
+    router.post("/logout", logout);
     router.get("/whoami", whoami);
 
     return router;
 };
 
 
+function getAuthMiddleware(redisClient) {
+    return async (req, res, next) => {
 
-const authMiddleware = (req, res, next) => {
+        const token = req.headers["authorization"]
 
-    const token = req.headers["authorization"]
+        if (!token) {
+            return res.status(403).json({message: "token is required."});
+        }
 
-    if (!token) {
-        return res.status(403).json({message:"token is required."});
+        try {
+            const val = await redisClient.get(token)
+            if (!val) {
+                return res.status(498).json({message: "token is expired."});
+            }
+        } catch (err) {
+            console.log("getting error failed: ", err)
+            return res.status(500).json({message: "internal error happend"})
+        }
+
+        try {
+            req.user = jwt.verify(token, process.env.AUTH_SALT);
+        } catch (err) {
+            return res.status(401).json({message: "token is invalid."});
+        }
+
+        next();
     }
-
-    try {
-        req.user = jwt.verify(token, process.env.AUTH_SALT);
-    } catch (err) {
-        return res.status(401).json({message:"token is invalid."});
-    }
-
-    next();
 }
 
 module.exports = Object.freeze({
-    authMiddleware,
+    getAuthMiddleware,
     login,
 });
